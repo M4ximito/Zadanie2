86695c5157039bd744b335fcc68aba5c592974d6
M4ximito <makssanchuk377@gmail.com>
2023-06-11 00:10:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Maksim Maltsau
+          Maksim Maltsau 12
         </p>
       </header>
     </div>
